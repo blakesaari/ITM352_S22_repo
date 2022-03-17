a850c6f0728000af12296b41de347708f0bb4fd0
Blake Saari <78383023+blakesaari@users.noreply.github.com>
2022-03-16 20:58:28 -1000

    Lab9

---
+++ Lab9/Ex6-c.js
@@ -0,0 +1,17 @@
+var picture_data = "image data:XOXOXO";
+
+function download(url, callBack) {
+    setTimeout(() => {
+        // script to download the picture here
+        console.log(`Downloading ${url} ...`);
+        picture_data = "image data:XOXOXO";
+    }, 3* 1000);
+    callBack(picture_data);
+}
+
+function process(picture) {
+    console.log(`Processing ${picture}`);
+}
+
+let url = 'https://www.example.com/big_pic.jpg';
+download(url, process);
--- Lab9/Ex6.js
+++ Lab9/Ex6.js
@@ -1,3 +1,9 @@
+var attributes = "Blake;21;20.5;-20.5";
+var parts_array = attributes.split(";");
+
+parts_array.forEach(checkIt);
+parts_array.forEach((item, index) => {console.log(`part ${index} is ${(AreStringsNonNegInt(item) ? 'a':'not a')}quantity`);})
+
 // The function allows us to check whether or not a string is not a number, has a negative value, and is an integer.
 function AreStringsNonNegInt(q, returnErrors=false) 
     {
@@ -5,16 +11,9 @@ function AreStringsNonNegInt(q, returnErrors=false)
     if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
     if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
     if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-
     return (returnErrors ? errors : (errors.length == 0));
     }
-
-var attributes = "Blake;21;20.5;-20.5";
-
-var parts_array = attributes.split(";");
-
-for(let part of parts_array)
+function checkIt (item, index) 
     {
-    let errs = AreStringsNonNegInt(part,true)
-    console.log(`Part ${part}; AreStringsNonNegInt ${errs.join(' ')}`);
+    console.log(`part ${index} is ${(AreStringsNonNegInt(item)?'a':'not a')} quantity`);
     }
