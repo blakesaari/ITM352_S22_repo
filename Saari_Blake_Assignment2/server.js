ab9f659a2adcadf483a711ef250237dae5e498f8
Blake Saari <78383023+blakesaari@users.noreply.github.com>
2022-04-15 16:27:33 -1000

    CRYING

--- Saari_Blake_Assignment2/server.js
+++ Saari_Blake_Assignment2/server.js
@@ -1,68 +1,145 @@
-// Based on Momoka Michimoto's (Fall 2021) server.js 
-
-// Determines valid quantity (If "q" is a negative interger)
-function isNonNegInt(q, return_errors = false) {
-    errors = []; // assume no errors at first
-    if (q == '') q = 0; // handle blank inputs as if they are 0
-    if (Number(q) != q) errors.push('<b><font color="red">Not a number!</font></b>'); // Check if string is a number value
-    if (q < 0) errors.push('<b><font color="red">Negative value!</font></b>'); // Check if it is non-negative
-    if (parseInt(q) != q) errors.push('<b><font color="red">Not an integer!</font></b>'); // Check that it is an integer
-    return return_errors ? errors : (errors.length == 0);
-};
-
-// Determines input in textbox
-function checkQuantityTextbox(qtyTextbox) {
-    errs = isNonNegInt(qtyTextbox.value, true);
-    if (errs.length == 0) errs = ['Want to purchase: '];
-    if (qtyTextbox.value.trim() == '') errs = ['Type desired quantity: '];
-    document.getElementById(qtyTextbox.name + '_label').innerHTML = errs.join('<font color="red">, </font>');
-};
-
-
-// Load Product Data
-    var products = require(__dirname + '/products.json');
-    // Initialize Quantity
-        products.forEach((prod,i)=>{prod.quantity_available = products[i].quantity_available})
 // Load Packages
 
     // Load Express Package
-        var express = require('express');
-        var app = express();
-    
+    var express = require('express');
+    var app = express();
+
     // Load File System Package
         var fs = require('fs')
 
     // Load Body-Parser Package
         var parser = require("body-parser");
-    
+
     // Load QueryString Package
         const qs = require('querystring');
         const { response } = require('express');
 
-// Load User Data
-    var user_data = require(__dirname + '/public/data/user_data.json');
-    // Store Purchase Data
-        var quantity_data = {};
+    // Load Product Data
+        var products = require(__dirname + '/products.json');
+        // Initialize Quantity
+            products.forEach((prod,i)=>{prod.quantity_available = products[i].quantity_available})
 
-// Taken from Lab 14
-if (fs.existsSync(user_data)) {
-    // have reg data file, so read data and parse into user_data_obj
-    var data = fs.readFileSync(user_data, 'utf-8'); // read the file and return its content.
-    var read_users_data = JSON.parse(data);
+    // Load User Data
+        var filename = require('./public/data/user_data.json');
+const validate = require('validate.js');
+const { URLSearchParams } = require('url');
+const { request } = require('http');
+
+        // Store Data from Purchase
+        var qty_obj = {};
+    
+    // Load Body
+        app.use(parser.urlencoded({extended: true}));
+
+// Determines valid quantity (If "q" is a negative interger)
+    function isNonNegInt(q, return_errors = false) {
+        errors = []; // assume no errors at first
+        if (q == '') q = 0; // handle blank inputs as if they are 0
+        if (Number(q) != q) errors.push('<b><font color="red">Not a number!</font></b>'); // Check if string is a number value
+        if (q < 0) errors.push('<b><font color="red">Negative value!</font></b>'); // Check if it is non-negative
+        if (parseInt(q) != q) errors.push('<b><font color="red">Not an integer!</font></b>'); // Check that it is an integer
+        return return_errors ? errors : (errors.length == 0);
+    };
+
+// Determines input in textbox
+    function checkQuantityTextbox(qtyTextbox) {
+        errs = isNonNegInt(qtyTextbox.value, true);
+        if (errs.length == 0) errs = ['Want to purchase: '];
+        if (qtyTextbox.value.trim() == '') errs = ['Type desired quantity: '];
+        document.getElementById(qtyTextbox.name + '_label').innerHTML = errs.join('<font color="red">, </font>');
+    };
+
+// ---------------------------- Log-in -------------------------------- // 
+
+if (fs.existsSync(filename)) {
+    // Lab 13 Example
+    var data_str = fs.readFileSync(filename, 'utf-8');
+    var user_str = JSON.parse(data_str);
+}
+else {
+    console.log(filename + ' does not exist.');
 }
 
-// Get Body
-    app.use(parser.urlencoded({extended: true}));
+// Processing Login Request
+    app.post("/process_login", function (request, response) {
+        var errors = {};
+        var the_email = request.body['email'].toLowerCase();
+        var the_password = request.body['password'];
 
-// Monitor all requests
+            if (typeof user_str[the_email] != 'undefined') {
+                if (user_str[the_email].password == the_password) {
+                    qty_obj['email'] = the_email;
+                    qty_obj['fullname'] = user_str[the_email].name;
 
-    app.all('*', function (request, response, next) {
-    console.log(request.method + ' to ' + request.path);
-    next();
+                    let params = new URLSearchParams(qty_obj);
+                    response.redirect('./invoice.html?' + params.toString());
+                    return;
+                } else {
+                    errors['login_error'] = `Incorrect Password!`;
+                }
+            } else {
+                errors['login_error'] = `Incorrect E-Mail Address!`;
+            }
+            let params = new URLSearchParams(errors);
+            params.append('email', data_str);
+            response.redirect('./login.html' + params.toString());
     });
 
-// Process purchase request (validate quantities, check quantity available)
+// ---------------------------- Register -------------------------------- // 
+    app.post("/process_registration", function(req, res){
+        console.log(request.body)
 
+        var registration_errors = {};
+        var registration_email = request.body['email'].toLowerCase();
+        var reigstration_password = request.body['password'];
+
+
+        // Name Validation
+        if(/^[A-Za-z, ]+$/.test(req.body.name)) {    
+        }
+        else {
+            registration_errors['name'] = `Only a-z characters allowed.`;
+        }
+
+        // Email Validation
+        if(ValidateEmail.test(registration_email)) {
+        }
+        else {
+            registration_errors['email'] = `Must enter a valid email address.`;
+        }
+
+        // Password Validation
+        if(reigstration_password < 6) {
+            registration_errors['password'] = `Minimum 6 character password.`
+        }
+
+            // Repeat Password Validation
+            if (reigstration_password != req.body.repeat_password) {
+                registration_errors[repeat_password] = 'Passwords do not match!';
+            }
+
+        if (object.keys(registration_errors).length == 0) {
+            console.log('No errors')
+            var email = registration_email;
+            user_str[registration_email] = {};
+            user_str[registration_email] = request.body.password;
+            user_str[registration_email].name = request.body.fullname;
+
+            fs.writeFileSync(filename, JSON.stringify(user_str), "utf-8");
+
+            qty_obj['email'] = registration_email
+            qty_obj['fullname'] = user_str[registration_email].name;
+            let params = new URLSearchParams(qty_obj);
+            response.redirect('./invoice.html' + params.toString());
+        } else {
+            request.body['registration_errors'] = JSON.stringify(registration_errors);
+            let params = new URLSearchParams(request.body);
+            response.redirect("./registration.html?" + params.toString());
+        }
+    });
+
+// ---------------------------- Purchase -------------------------------- // 
+// Processing Purchase Request (Validates Quantities & Checks Availability)
     app.post("/purchase", function(request, response, next) {
         var quantities = request.body['quantity'];
         var errors = {};
@@ -90,7 +167,8 @@ if (fs.existsSync(user_data)) {
         for (i in quantities) {
             products[i].quantity_available -= Number(quantities[i]);
         }
-        response.redirect('./login.html?' + qs.stringify(quantity_object));
+        qty_obj = quantity_object
+        response.redirect('./login.html?');
     }
         else {
             let errors_obj = { "errors": JSON.stringify(errors) };
@@ -99,43 +177,13 @@ if (fs.existsSync(user_data)) {
         }
     });
 
-//--------Login Page Processing--------
-
-    // Process Login
-    app.post("/login_process", function (request, response) {
-        // Process login and redirect if logged in, return to login if failed
-        var user_email = request.body['email'].toLowerCase();
-        var user_password = request.body['password'];
-        var errors = {};
-        
-        if (typeof read_users_data[user_email] != 'undefined') {
-            if (read_users_data[user_email].password == user_password) {
-
-                request.query['email'] = user_email
-                response.redirect('./invoice.html?' + qs.stringify(request.query));
-                return;
-            } else {
-                errors['login_erorr'] = `Wrong password!`;
-            }
-        }
-            else {
-                errors['login_erorr'] = `${user_email} is not registered!`;
-            }
-
-            // Redirect With Error Message
-            let params = new URLSearchParams(errors);
-            params.append('email', user_email);
-            response.redirect(`./login.html?` + params.toString());
-    });
-
-
 // Routing 
-    app.get("/products.json", function(request, response, next)
-        {
-            response.type('.js');
-            var products_str = `var products = ${JSON.stringify(products)};`;
-            response.send(products_str);
-        });
+app.get("/products.json", function(request, response, next)
+    {
+        response.type('.js');
+        var products_str = `var products = ${JSON.stringify(products)};`;
+        response.send(products_str);
+    });
     
 // Route all other GET requests to files in public 
     app.use(express.static(__dirname + '/public'));
