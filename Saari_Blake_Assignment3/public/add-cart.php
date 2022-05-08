fd4cc1a2b829ffb25aad4f88ba522f02cf06481c
Blake Saari <saariblake1@gmail.com>
2022-05-07 22:17:54 -1000

    s22

--- Saari_Blake_Assignment3/public/add-cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost:8889", "root", "root", "classicmodels");
-
-$quantity = $_POST["quantity"];
-$productCode = $_POST["productCode"];
-
-$cart = isset($_COOKIE["cart"]) ? $_COOKIE["cart"] : "[]";
-$cart = 
