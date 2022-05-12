5359568794ce1e12d68f9aedd1c00d13c5c650f8
Blake Saari <saariblake1@gmail.com>
2022-05-11 23:06:33 -1000

    Lab13

---
+++ Lab13/Ex1a.js
@@ -0,0 +1,8 @@
+
+var users_reg_data = require("./user_data.json");
+
+console.log(users_reg_data);
+
+console.log(typeof users_reg_data);
+
+console.log(users_reg_data["kazman"].password);
--- Lab13/Ex4.js
+++ Lab13/Ex4.js
@@ -16,7 +16,6 @@ if(fs.existsSync(filename)) {
 } else {
     console.log(`${filename} does not exist!`);
 }
-
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/login", function (request, response) {
@@ -33,18 +32,51 @@ app.get("/login", function (request, response) {
     response.send(str);
  });
 
-app.post("/login", function (request, response) {
+ app.post("/login", function (request, response) {
     console.log(request.body);
-    let username = request.body.username
-    users[username] = {};
+    // Process login form POST and redirect to logged in page if ok, back to login page if not
+    if(typeof users[request.body.username] != 'undefined') {
+    // Username exists, get stored password & check if it matches
+        if(users[request.body.username].password == request.body.password) {
+        response.send(`${request.body.username} is logged in`)
+        }
+        else {
+        response.send(`Password invalid! ${str}`)
+        }
+        } else {
+        response.send(`${request.body.username} does not exist! ${str}`)
+    }
+});
+
+
+app.get("/register", function (request, response) {
+    // Give a simple register form
+    str = `
+<body>
+<form action="" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
+ });
+
+ app.post("/register", function (request, response) {
+    // process a simple register form
+    console.log(request.body);
+    let username = request.body.username;
+    if (users[username] = {}) {
     users[username].password = request.body.password;
     users[username].email = request.body.email;
+    fs.writeFileSync(filename, JSON.stringify(user_data));
+    response.redirect("/login");
+}
+else (response.redirect("/register"))
+ });
 
-    fs.writeFileSync(filename, JSON.stringify(users))
-
-    // Process login form POST and redirect to logged in page if ok, back to login page if not
-   
-    
-});
 
 app.listen(8080, () => console.log(`listening on port 8080`));
